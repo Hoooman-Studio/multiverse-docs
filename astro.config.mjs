fc7d0871e4cdb85bf90645b0cc7e6f29dae416d4
Hooman-Studio <development@hoomanstudio.com>
2025-05-19 10:05:33 -0700

    Update Astro configuration and integrate Tailwind CSS
    - Modified `astro.config.mjs` to set the site title and logo, added custom CSS, and integrated Tailwind CSS.
    - Updated `package.json` to include new dependencies for Tailwind CSS and font support.
    - Added new font files and a favicon, while removing the old SVG favicon.
    - Created global and font stylesheets to support the new design.
    - Updated the documentation landing page to reflect the new branding and structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,49 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://docs.multiversecomputing.com",
+  integrations: [
+    starlight({
+      title: "Multiverse",
+      logo: {
+        src: "./src/assets/multiverse-logo.png",
+        replacesTitle: true,
+      },
+      customCss: [
+        "./src/styles/global.css",
+        "./src/styles/fonts.css",
+        "@fontsource-variable/jetbrains-mono",
+      ],
+      social: [
+        {
+          icon: "youtube",
+          label: "Youtube",
+          href: "https://www.youtube.com/@multiversecomputing",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      favicon: "favicon.ico",
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
